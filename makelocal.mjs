b5f017e7d201f782e5a549947780bb09ceaf7d26
Zbynek Konecny <zbynek@geogebra.at>
2024-04-10 17:51:22 +0200

    Add reference

---
+++ makelocal.mjs
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+// Read YAML file
+const inputFilePath = 'antora-playbook.yml';
+const outputFilePath = 'playbook-local.yml';
+
+const yamlData = fs.readFileSync(inputFilePath, 'utf8');
+const parsedData = yaml.load(yamlData);
+parsedData.content.sources[0].url = '../manual'
+parsedData.content.sources[1].url = '../integration'
+// Write parsed YAML data to another file
+const yamlString = yaml.dump(parsedData);
+fs.writeFileSync(outputFilePath, yamlString, 'utf8');
+
+console.log('YAML file successfully written to', outputFilePath);
